5861090b89e44766e47fe1dd8eddcffa4822cc30
fpalfonso <fpalfonso@up.edu.ph>
2024-04-18 01:11:19 +0800

    contract can create message and added a test

--- programs/upd_sbc/src/lib.rs
+++ programs/upd_sbc/src/lib.rs
@@ -6,10 +6,31 @@ declare_id!("GCGcqanABcRwwfVXyma59p8H1mX7KPNLwodbdG9Ee1g7");
 pub mod upd_sbc {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_message(ctx: Context<CreateMessage>, content: String) -> Result<()> {
+        let message: &mut Account<Message> = &mut ctx.accounts.message;
+        let author: &Signer = &ctx.accounts.author;
+        let clock: Clock = Clock::get().unwrap();
+    
+        message.author = *author.key;
+        message.timestamp = clock.unix_timestamp;
+        message.content = content;
+    
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateMessage<'info> {
+    #[account(init, payer = author, space = 1000)]
+    pub message: Account<'info, Message>,
+    #[account(mut)]
+    pub author: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Message {
+    pub author: Pubkey,
+    pub timestamp: i64,
+    pub content: String,
+}
--- tests/upd_sbc.ts
+++ tests/upd_sbc.ts
@@ -1,16 +1,38 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { UpdSbc } from "../target/types/upd_sbc";
+import * as assert from "assert";
 
 describe("upd_sbc", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.UpdSbc as Program<UpdSbc>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+  it("Can create a message", async () => {
+    const message = anchor.web3.Keypair.generate();
+    const messageContent = "Hello World!";
+    const tx = await program.methods
+      .createMessage(messageContent)
+      .accounts({
+        message: message.publicKey,
+        author: provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([message])
+      .rpc();
     console.log("Your transaction signature", tx);
+
+    const messageAccount = await program.account.message.fetch(
+      message.publicKey
+    );
+
+    assert.equal(
+      messageAccount.author.toBase58(),
+      provider.wallet.publicKey.toBase58()
+    );
+    assert.equal(messageAccount.content, messageContent);
+    assert.ok(messageAccount.timestamp);
   });
 });
